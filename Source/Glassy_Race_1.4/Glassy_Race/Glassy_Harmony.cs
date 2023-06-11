ee3b36e7b0106a83db0bdd420ab19b8f2600c5ee
Aoi_Nasuko <aoinasuko.nasunasu@gmail.com>
2023-06-12 00:20:03 +0900

    0612 Fix

--- Source/Glassy_Race_1.4/Glassy_Race/Glassy_Harmony.cs
+++ Source/Glassy_Race_1.4/Glassy_Race/Glassy_Harmony.cs
@@ -24,4 +24,34 @@ namespace Glassy_Race
         }
     }
 
+    // テイム出来るようにする
+    [HarmonyPatch(typeof(TameUtility), "CanTame")]
+    [HarmonyPatch(new Type[]
+    {
+        typeof(Pawn),
+    })]
+    internal class Glassy_FixCanTame
+    {
+        [HarmonyPrefix]
+        private static bool Prefix(ref bool __result, Pawn pawn)
+        {
+            if (pawn.def.defName == "Glassy_Pawn")
+            {
+                Faction glassy_faction = FactionUtility.DefaultFactionFrom(Faction_Glassy.Glassy_WildGlassy);
+                if (glassy_faction != null)
+                {
+                    if (pawn.Faction != null)
+                    {
+                        if (pawn.Faction == glassy_faction)
+                        {
+                            __result = true;
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
 }
