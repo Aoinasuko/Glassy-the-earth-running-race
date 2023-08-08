fdc193f0d85ee8f5ecb6dd1ac86ecf3a65d70c1c
Aoi_Nasuko <aoinasuko.nasunasu@gmail.com>
2023-08-08 20:55:15 +0900

    0808

--- Source/Glassy_Race_1.4/Glassy_Race/Glassy_Config.cs
+++ Source/Glassy_Race_1.4/Glassy_Race/Glassy_Config.cs
@@ -29,7 +29,7 @@ namespace Glassy_Race
 	public class Glassy_Config : ModSettings
 	{
 		// バージョン
-		public static int ver = 200;
+		public static int ver = 210;
 
 		// アップデートバージョン
 		public static int Updatever = 0;
